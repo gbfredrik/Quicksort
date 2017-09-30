da63839fd2c5d90798610598a5c4a0a07d34e693
Fredrik Gerdin Börjesson <frebo147@student.liu.se>
2017-09-30 15:55:55 +0200

    dsafghjkj

--- src/QSortTooSlow.java
+++ src/QSortTooSlow.java
@@ -1,4 +1,4 @@
-public class QSortBad {
+public class QSortTooSlow {
 
 	/**
 	 * Quicksort the array a[] using m as cutoff to insertion sort.
@@ -15,75 +15,56 @@ public class QSortBad {
 		if (high <= low + m) {
 			insertionsort(a, low, high);
 			return;
-		}/* else if (high - low + 1 < 2) {
-			return;
-		}*/
+		}
 		int j = partitition(a, low, high);
 		quicksort(a, low, j - 1, m);
 		quicksort(a, j + 1, high, m);
 	}
 
-	// http://www.ida.liu.se/opendsa/OpenDSA/Books/TDDC91F17/html/Quicksort.html#QuicksortPivotPRO
 	private static int partitition(int[] a, int low, int high) {
-		int pivotIndex = median(a, low + 0, high - 0);
+		int pivotIndex = median(a, low, (low + high) / 2, high);
 		int pivot = a[pivotIndex];
-		//System.out.println("median = " + a[pivot]);
+		swap(a, pivotIndex, high);
 		int tempL = low;
-		/*// 10s slower!
-		int tempH = high - 1;
-		while (true) {
-			while (a[++tempL] < pivot) {
-			}
-			while (a[--tempH] > pivot) {
-			}
-			if (tempL >= tempH) {
-				break;
-			}
-			swap(a, tempL, tempH);
-		}
-		*/
-		// Faster
-		swap(a, pivotIndex, high - 0);
+		pivotIndex = high;
+
+		//      int tempH = high - 1;
+		//		while (true) {
+		//			while (a[++tempL] < pivot) {
+		//			}
+		//			while (a[--tempH] > pivot) {
+		//			}
+		//			if (tempL >= tempH) {
+		//				break;
+		//			}
+		//			swap(a, tempL, tempH);
+		//		}
+
 		for (int i = low; i <= (high - 1); i++) {
 			if (a[i] < pivot) {
-	            swap(a, i, tempL);
-	            tempL++;
+				swap(a, i, tempL);
+				tempL++;
 			}
 		}
-		//swap(a, tempL, high - 1);
 		swap(a, pivotIndex, tempL);
 		return tempL;
 	}
-	
-	private static int median(int[] a, int low, int high) {
-		int middle = (low + high) / 2;
-		/*if (a[low] > a[middle]) {
-			swap(a, low, middle);
-		}
-		if (a[low] > a[high]) {
-			swap(a, low, high);
-		}
-		if (a[middle] > a[high]) {
-			swap(a, middle, high);
-		}
-		swap(a, middle, high - 1);
-		return a[high - 1];*/
+
+	private static int median(int[] a, int low, int middle, int high) {
 		if (a[low] > a[middle]) {
 			if (a[high] > a[low]) {
 				return low;
-			} else if(a[high] < a[low]) {
+			} else if (a[high] < a[middle]) {
 				return middle;
-			} else {
+			} else
 				return high;
-			}
 		} else {
 			if (a[high] > a[middle]) {
 				return middle;
 			} else if (a[high] < a[low]) {
 				return low;
-			} else {
+			} else
 				return high;
-			}
 		}
 	}
 
@@ -96,16 +77,15 @@ public class QSortBad {
 	/**
 	 * Sort from a[low] to a[high] using insertion sort.
 	 */
-	public static void insertionsort(int[] a, int low, int high) {
+	private static void insertionsort(int[] a, int low, int high) {
 		for (int i = low + 1; i <= high; i++) {
 			int temp = a[i];
 			int j = i;
 			while (j > low && temp < a[j - 1]) {
-				a[j] = a[j-1];
+				a[j] = a[j - 1];
 				j--;
 			}
 			a[j] = temp;
 		}
 	}
 }
-// http://www.ida.liu.se/~TDDC91/info/misc/labbar17.pdf#page=15
